7d991b8917da2eab01da9ecd7b3819f557d56974
shireen123456 <168181609+shireen123456@users.noreply.github.com>
2024-04-27 05:59:44 +0700

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,79 +1,105 @@
-"use client";
-import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import { SkillData } from '@/constants';
-import Image from "next/image";
-import { Autoplay } from "swiper/modules";
+"use client"
 
-const Page = () => {
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
   return (
-    <div
-      style={{ backgroundImage: "url(/bg-2.jpg)" }}
-      className="h-screen w-screen flex items-center justify-center bg-cover bg-center"
-    >
-      <div className="flex flex-col gap-20 max-w-[80%] text-center items-center">
-        <div className="flex flex-col items-center gap-4">
-          <h1 className="font-semibold text-white text-[50px]">
-            Skills{" "}
+    <main className="w-screen h-screen relative">
+      <div
+        className="flex items-center w-full h-full bg-cover bg-center"
+        style={{ backgroundImage: "url(./main-bg.webp)" }}
+      >
+        <div className="pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
+          <h1 className="text-[50px] text-white font-semibold">
+            Make anything possible with
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
               {" "}
-              &{" "}
+              Web Development
             </span>
-            Technologies
           </h1>
-          <p className="text-gray-400 text-[20px]">
-            Using the latest tech this world has to offer
+          <p className="text-gray-200 hidden md:block">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
+          <div className="flex-col md:flex-row hidden md:flex gap-5">
+            <Link
+              href="./my-skills"
+              className="rounded-[20px] group relative bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
+            >
+              Learn more
+            </Link>
+            <Link
+              href="./my-projects"
+              className="rounded-[20px] group relative bg-trasparent px-5 border border-white py-3 text-lg text-white max-w-[200px]"
+            >
+              <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hver:opacity-20" />
+              My projects
+            </Link>
+            <Link
+              href="./contact-me"
+              className="rounded-[20px] group relative bg-trasparent border border-white px-5 py-3 text-lg text-white max-w-[200px]"
+            >
+              <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hver:opacity-20" />
+              Contact me
+            </Link>
+          </div>
         </div>
-        <Swiper
-          slidesPerView={5}
-          loop={true}
-          autoplay={{
-            delay: 0,
-            disableOnInteraction: false,
-          }}
-          speed={5000}
-          modules={[Autoplay]}
-          className="max-w-[80%]"
-        >
-          {SkillData.map((skill, index) => (
-            <SwiperSlide key={index}>
-              <Image
-                src={skill.Image}
-                alt={skill.name}
-                width={skill.width}
-                height={skill.height}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-        <Swiper
-          slidesPerView={5}
-          loop={true}
-          autoplay={{
-            delay: 0,
-            disableOnInteraction: false,
-            reverseDirection: true
-          }}
-          speed={5000}
-          modules={[Autoplay]}
-          className="max-w-[80%]"
-        >
-          {SkillData.map((skill, index) => (
-            <SwiperSlide key={index}>
-              <Image
-                src={skill.Image}
-                alt={skill.name}
-                width={skill.width}
-                height={skill.height}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
       </div>
-    </div>
-  );
-};
 
-export default Page;
+      <div className="absolute flex bottom-10 z-[20] right-5 flex-col md:hidden gap-5">
+        <Link
+          href="./my-skills"
+          className="rounded-[20px] group bg-blue-500 px-5 py-3 text-lg text-white max-w-[200px]"
+        >
+          Learn more
+        </Link>
+
+        <Link
+          href="/my-projects"
+          className="rounded-[20px] group bg-trasparent border border-white px-5 py-3 text-lg text-white max-w-[200px]"
+        >
+          My projects
+        </Link>
+        <Link
+          href="./contact-me"
+          className="rounded-[20px] group bg-trasparent border border-white px-5 py-3 text-lg text-white max-w-[200px]"
+        >
+          Contact me
+        </Link>
+      </div>
+
+      <div className="absolute bottom-0 right-0 z-[10]">
+        <Image
+          src="./horse.png"
+          alt="horse"
+          height={300}
+          width={300}
+          className="absolute right-55 top-40"
+        />
+
+        <Image src="./cliff.webp" alt="cliff" width={480} height={480} />
+      </div>
+
+      <div className="absolute bottom-0 z-[5] w-full h-auto">
+        <Image
+          src="./trees.webp"
+          alt="trees"
+          width={2000}
+          height={2000}
+          className="w-full h-full"
+        />
+      </div>
+
+      <Image
+        src="./stars.png"
+        alt="stars"
+        height={300}
+        width={300}
+        className="absolute top-0 left-0 z-[10]"
+      />
+    </main>
+  );
+}
+
+
